a73c45403d807392c19d8dc891941a67c415605d
Frizzle <tommaso.daddio@icloud.com>
2025-12-10 11:36:12 +0100

    Add script to generate index.html from plist files
    This script generates an index.html file from plist files in the output folder, using a template.

---
+++ generate_index.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+
+GITHUB_USER="FrizzleM"
+GITHUB_REPO="BreakFree"
+PLIST_FOLDER="output"
+
+mkdir -p "$PLIST_FOLDER"
+
+BASE_URL="https://raw.githubusercontent.com/$GITHUB_USER/$GITHUB_REPO/main/$PLIST_FOLDER"
+
+TEMPLATE="template.html"
+OUTPUT="index.html"
+
+plist_blocks=""
+
+for plist in $(ls "$PLIST_FOLDER"/*.plist 2>/dev/null | sort); do
+    filename=$(basename "$plist")
+    name="${filename%.plist}"
+
+    plist_blocks+="<div class=\"plist-item\">
+<strong>$name</strong><br>
+<a href=\"itms-services://?action=download-manifest&url=$BASE_URL/$filename\">
+Install $name
+</a>
+</div>
+
+"
+done
+
+sed "s|{{PLIST_BLOCKS}}|$plist_blocks|" "$TEMPLATE" > "$OUTPUT"
+echo "Generated $OUTPUT"
