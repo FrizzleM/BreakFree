2296394ba4e8d6a4294a4c69f1d85ca055e8a14c
Frizzle <tommaso.daddio@icloud.com>
2025-12-10 13:24:20 +0100

    Handle empty installer list

--- generate_index.sh
+++ generate_index.sh
@@ -14,19 +14,21 @@ OUTPUT="index.html"
 
 plist_blocks=""
 
-for plist in $(ls "$PLIST_FOLDER"/*.plist 2>/dev/null | sort); do
-    filename=$(basename "$plist")
-    name="${filename%.plist}"
+if compgen -G "$PLIST_FOLDER"/*.plist > /dev/null; then
+    for plist in $(ls "$PLIST_FOLDER"/*.plist 2>/dev/null | sort); do
+        filename=$(basename "$plist")
+        name="${filename%.plist}"
 
-    plist_blocks+="<div class=\"plist-item\">
-<strong>$name</strong><br>
-<a href=\"itms-services://?action=download-manifest&url=$BASE_URL/$filename\">
-Install $name
-</a>
-</div>
-
-"
-done
+        plist_blocks+="<div class=\"plist-item\">\n"
+        plist_blocks+="<strong>$name</strong><br>\n"
+        plist_blocks+="<a href=\"itms-services://?action=download-manifest&url=$BASE_URL/$filename\">\n"
+        plist_blocks+="Install $name\n"
+        plist_blocks+="</a>\n"
+        plist_blocks+="</div>\n\n"
+    done
+else
+    plist_blocks+="<div class=\"plist-item\">No signed installers are available right now.</div>\n"
+fi
 
 sed "s|{{PLIST_BLOCKS}}|$plist_blocks|" "$TEMPLATE" > "$OUTPUT"
 echo "Generated $OUTPUT"
