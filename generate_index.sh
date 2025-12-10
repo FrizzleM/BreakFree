b05f86f7326ec6dbc1f0d3bce263e362ee898c57
Frizzle <tommaso.daddio@icloud.com>
2025-12-10 12:35:00 +0000

    Fixed the generate html script

--- generate_index.sh
+++ generate_index.sh
@@ -3,9 +3,7 @@ set -e
 
 GITHUB_USER="FrizzleM"
 GITHUB_REPO="BreakFree"
-PLIST_FOLDER="output"
-
-mkdir -p "$PLIST_FOLDER"
+PLIST_FOLDER="Feather/output"
 
 BASE_URL="https://raw.githubusercontent.com/$GITHUB_USER/$GITHUB_REPO/main/$PLIST_FOLDER"
 
@@ -14,21 +12,19 @@ OUTPUT="index.html"
 
 plist_blocks=""
 
-if compgen -G "$PLIST_FOLDER"/*.plist > /dev/null; then
-    for plist in $(ls "$PLIST_FOLDER"/*.plist 2>/dev/null | sort); do
-        filename=$(basename "$plist")
-        name="${filename%.plist}"
+for plist in $(ls "$PLIST_FOLDER"/*.plist 2>/dev/null | sort); do
+    filename=$(basename "$plist")
+    name="${filename%.plist}"
 
-        plist_blocks+="<div class=\"plist-item\">\n"
-        plist_blocks+="<strong>$name</strong><br>\n"
-        plist_blocks+="<a href=\"itms-services://?action=download-manifest&url=$BASE_URL/$filename\">\n"
-        plist_blocks+="Install $name\n"
-        plist_blocks+="</a>\n"
-        plist_blocks+="</div>\n\n"
-    done
-else
-    plist_blocks+="<div class=\"plist-item\">No signed installers are available right now.</div>\n"
-fi
+    plist_blocks+="<div class=\"plist-item\">
+<strong>$name</strong><br>
+<a href=\"itms-services://?action=download-manifest&url=$BASE_URL/$filename\">
+Install $name
+</a>
+</div>
+
+"
+done
 
 sed "s|{{PLIST_BLOCKS}}|$plist_blocks|" "$TEMPLATE" > "$OUTPUT"
 echo "Generated $OUTPUT"
